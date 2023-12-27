14e1abb10336213b51e958c8f8da080a740ac932
muh-ansar <113844496+muh-ansar@users.noreply.github.com>
2023-12-27 21:29:42 +0800

    Update Kepiting.java

--- Kepiting.java
+++ Kepiting.java
@@ -12,7 +12,7 @@ public class Kepiting extends Actor
      * Act - do whatever the Kepiting wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void action()
+    public void act()
 {
 setLocation(getX() - 2, getY());
 if (getX() <= 0)
